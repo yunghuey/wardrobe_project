fa39ddc907a87c0784227f61021d4849d3bd26b0
YUNG HUEY TONG <yunghuey2002@gmail.com>
2024-05-23 22:13:17 +0800

    ADD valid unique to username and email

--- account/views.py
+++ account/views.py
@@ -12,6 +12,18 @@ def registerUser(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         validated_data = serializer.validated_data
+        
+        db = firestore.client()
+        user_table = db.collection('user')
+        # check for existing user with the same email
+        existing_email = user_table.where('email', '==', validated_data['email']).get()
+        if existing_email:
+            return Response({'error':'Email already exists.'}, status=400)
+        
+        existing_username = user_table.where('username', '==', validated_data['username']).get()
+        if existing_username:
+            return Response({'error':'Username already exists.'}, status=400)
+        
         validated_data['password'] = make_password(validated_data['password'])
         validated_data['is_logged'] = True
         
@@ -19,8 +31,6 @@ def registerUser(request):
         current_datetime = datetime.datetime.now(timezone)
         validated_data['created_date'] = current_datetime
         
-        db = firestore.client()
-        user_table = db.collection('user')
         user_document_ref = user_table.add(serializer.validated_data)[1]
        
         return Response({'id': user_document_ref.id}, status=201)
