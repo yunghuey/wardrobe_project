62bda10dc3fc366f513c576786912bf8dc247203
YUNG HUEY TONG <yunghuey2002@gmail.com>
2024-05-24 09:33:56 +0800

    can update, logout, create, read user

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,9 @@ from . import views #get all function in views
 
 urlpatterns = [
     path('add/', views.registerUser),
+    path('logout/', views.logoutUser),
+    path('get/<str:user_id>', views.getUserDetail),
+    path('update/<str:user_id>', views.updateDetail)
+    
     # path('getGarment/<str:garment_id>', views.getGarment),
 ]
--- account/views.py
+++ account/views.py
@@ -23,7 +23,7 @@ def registerUser(request):
         existing_username = user_table.where('username', '==', validated_data['username']).get()
         if existing_username:
             return Response({'error':'Username already exists.'}, status=400)
-        
+
         validated_data['password'] = make_password(validated_data['password'])
         validated_data['is_logged'] = True
         
@@ -34,4 +34,58 @@ def registerUser(request):
         user_document_ref = user_table.add(serializer.validated_data)[1]
        
         return Response({'id': user_document_ref.id}, status=201)
+    # take note to save id into SharedPreference 
     return Response(serializer.errors, status=400)
+
+@api_view(['PUT'])
+def logoutUser(request):
+    try:
+        db = firestore.client()
+        user_table = db.collection('user')
+        user_row = user_table.document(request.data.get('id'))
+        user_row.update({'is_logged': False})
+        return Response({'message': 'successfully logged out'}, status=200)
+    except Exception as e:
+        return Response({'error': str(e)}, status=400)
+
+@api_view(['GET'])
+def getUserDetail(request, user_id):
+    try:
+        db = firestore.client()
+        user_table = db.collection('user')
+        user_row = user_table.document(user_id)
+        doc_snapshot = user_row.get()
+        if doc_snapshot.exists:
+            user_data = doc_snapshot.to_dict()
+            user_data['id'] = user_id
+            return Response({'user': user_data}, status=200)
+        else:
+            return Response({'error': 'User not found'}, status=404)
+    except Exception as e:
+        return Response({'error':str(e)}, status=400)
+    
+@api_view(['PUT'])
+def updateDetail(request, user_id):
+    try:
+        db = firestore.client()
+        user_table = db.collection('user')
+        user_row = user_table.document(user_id)
+        user_data = user_row.get().to_dict()
+        print(user_data)
+        # print(user_data.where('is_logged', '==',True))
+        if user_data['is_logged'] == True:
+            if user_data['email'] == request.data.get('email'):
+                return Response({'error':'Email already exists.'}, status=400)
+            
+            if user_data['username'] == request.data.get('username'):
+                return Response({'error':'Username already exists.'}, status=400)
+            user_data.update(request.data)
+            user_row.set(user_data)
+            return Response(user_data, status=200)
+        return Response({'error': 'User is not logged in'}, status=409)
+    except Exception as e:
+        return Response({'error': str   (e)}, status=400)
+    
+# def login(request):
+    
+    
